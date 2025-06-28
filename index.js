7e2f5d47eca54c07fa2d06728aa823723a2d04f2
shihab-shamim <shihabshamim767@gmail.com>
2025-06-29 00:13:31 +0600

    all the jwt comlete

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const port=process.env.PORT || 5000
 const app=express()
+const cookieParser = require('cookie-parser')
 require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
 
 app.use(cors({
   origin:"http://localhost:5173",
   credentials:true
 }));
 app.use(express.json());
+app.use(cookieParser())
+
+const verifyToken=async(req,res,next)=>{
+  // console.log("hello");
+   const token = req.cookies?.accessToken;
+   if (!token) {
+    // Return the response immediately, so the code doesn't execute further
+    return res.status(401).send({ message: "forbidden access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decode) => {
+    if (err) {
+      // Return the response immediately in case of error
+      return res.status(401).send({ message: "Not Allow" });
+    }
+
+    req.user = decode;
+    // Proceed to the next middleware if token is valid
+    next();
+  });
+// next()
+}
 
 
 
@@ -41,14 +64,14 @@ async function run() {
      // jwt  related
      app.post("/jwt",async(req,res)=>{
       const user=req.body
+      console.log(user?.userEmail,"hhhhh");
       const token=jwt.sign(user?.userEmail,process.env.ACCESS_TOKEN,{
         expiresIn:"7d"
       })
       res.cookie("accessToken",token,{
         httpOnly:true,
-        secure:false,
-         sameSite: 'lax',
-         maxAge: 24 * 60 * 60 * 1000 
+        secure: isProduction?true:false,
+            sameSite: isProduction ? 'None' : 'Lax',
         
       }).send({ success: true })
      })
@@ -58,8 +81,9 @@ async function run() {
 
       res.clearCookie("accessToken",{
         httpOnly: true,
-     secure: false,
-     sameSite: 'lax'
+     secure: isProduction?true:false,
+            sameSite: isProduction ? 'None' : 'Lax',
+          
       }).send({ success: true })
 
      })
@@ -78,8 +102,9 @@ async function run() {
       res.send(result)
 
     })
-    app.get("/user",async(req,res)=>{
+    app.get("/user",verifyToken,async(req,res)=>{
       const email=req?.query?.email;
+        console.log('Cookies: ', req.user)
        const result=await userCollection.findOne({email:email})
       res.send(result)
 
@@ -96,6 +121,13 @@ async function run() {
       res.send(result)
 
     })
+    // app.get("/hello",async(req,res)=>{
+    //   const result={
+    //     name:"shihab",age:"21"
+    //   }
+
+    //   res.send(result)
+    // })
 
 
 
